0c7af43eb2e8aafbeb687aca67db11eec56b9bd7
Alex Arriaga <alexzander.arriaga@base22.com>
2018-10-05 10:12:19 -0500

    Agregar controlador de issues

--- database/migrations/2018_10_05_143824_create_projects_table.php
+++ database/migrations/2018_10_05_143824_create_projects_table.php
@@ -15,6 +15,16 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
