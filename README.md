94593284d02c0f0d266ea6718423093aa06641a2
Alex Arriaga <alexzander.arriaga@base22.com>
2018-10-09 15:15:49 -0500

    Agregar base de datos

---
+++ README.md
@@ -0,0 +1,128 @@
+# Curso Profesional Lumen + React
+
+# Project Manager API
+
+Un ejemplo de RESTful API creado con Laravel Lumen.
+
+## Pre-requisitos
+
+1) Este RESTful API fue creado con [Laravel Lumen](https://lumen.laravel.com/), que nos exige una versión moderna de PHP y algunas de sus extensiones instaladas:
+
+```
+PHP >= 7.1.3
+OpenSSL PHP Extension
+PDO PHP Extension
+Mbstring PHP Extension
+```
+
+Para desarrollo y configuración rápida te recomendamos instalar un meta-paquete como XAMPP 
+[descargar aquí](https://www.apachefriends.org/download.html). Sólo hay que estar seguros de descargar
+XAMPP con PHP 7.1 (recomendado). Esto te instalará MySQL, PHPMyAdmin, Apache y claro un PHP moderno.
+
+2) También necesitaremos composer ([descargar aquí](https://getcomposer.org/)) para descargar las dependencias de [Laravel Lumen](https://lumen.laravel.com/).
+
+3) Algo que recomendamos es instalar un cliente para probar todos tus *end-points*. 
+Nuestra herramienta favorita para tal propósito es [Postman](https://www.getpostman.com/) que tiene una
+aplicación gratuita para Windows, GNU/Linux y OS X. Después de instalar Postman puedes **importar** una colección
+de *end-points* que hemos creado para ti y que está disponible en: `<REPO>/webtraining/Project Manager API.postman_collection`.
+
+Una vez importada tu colección tendrás acceso a todos los servicios de Lumen como en la siguiente imagen:
+
+![Postman](https://raw.githubusercontent.com/webtrainingmx/rest-api-project-manager-junio-2017/master/webtraining/img/postman-get-users.png)
+
+
+## Instalación para Desarrollo
+
+1) Instalar dependencias de Composer (ejecutar desde el directorio raiz de este proyecto).
+```
+composer install
+```
+2) Configurar base de datos:
+
+Para tu comodidad hemos creado un *MySQL dump* en este archivo `<REPO>/database/sql/project_manager_api.sql`.
+Este archivo contiene dos usuarios, un proyecto y una tarea de demostración.
+
+2.1) Importa esta base de datos usando algún cliente web como PHPMyAdmin o Sequel Pro.
+2.2) Crea un usuario que se pueda conectar a esta base de datos, por ejemplo:
+```
+Base de datos:  project_manager_api
+Usuario:        project_mgr_user
+Constraseña:    D5xNL5LpHPVTxwz4
+```
+
+2.3) Crea un archivo llamado `.env` en la raíz de este proyecto, con los siguientes datos:
+```
+APP_ENV=local
+APP_DEBUG=true
+APP_KEY=base64:L9X69/CcxQHmSaxJq7cFFmtoJSUJBYla4WiypRCHPDI=
+APP_TIMEZONE=UTC
+
+LOG_CHANNEL=stack
+LOG_SLACK_WEBHOOK_URL=
+
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=project_manager_api
+DB_USERNAME=project_mgr_user
+DB_PASSWORD=D5xNL5LpHPVTxwz4
+
+CACHE_DRIVER=file
+QUEUE_CONNECTION=sync
+```
+
+Lo importante en este caso son los datos de conexión a la base de datos y la generación de tu `APP_KEY` que puedes 
+generar usando este [Random Generator](https://webtraining.zone/random-generator).
+
+
+**Nota para MySQL 8**
+
+Si estás usando MySQL 8, la forma de creación de tu base de datos es como sigue:
+
+```
+CREATE SCHEMA `lumen_projects_db` DEFAULT CHARACTER SET utf8 ;
+```
+
+3) Iniciar tu servidor en el puerto 8085
+```
+php -S localhost:8085 -t public
+```
+
+## Preguntas Frecuentes
+
+**¿Por qué cuando visito un *end-point*, por ejemplo `/users` veo un código JSON de "Unauthorized"?**
+
+Recuerda que todos los *end-points* de nuestro RESTful API sólo pueden ser llamados utilizando
+el *header* **Content-Type** como **application/json** (es decir, con llamados AJAX
+debería funcionar correctamente).
+
+
+**Cuando intento arrancar el servidor en el puerto 8085 me dice que el puerto ya está ocupado ¿cómo lo soluciono?**
+
+Simplemente cambia el puerto de conexión, por ejemplo si queremos arrancar el servidor en el puerto 8089:
+```
+php -S localhost:8089 -t public
+```
+
+
+## Lumen PHP Framework
+
+[![Build Status](https://travis-ci.org/laravel/lumen-framework.svg)](https://travis-ci.org/laravel/lumen-framework)
+[![Total Downloads](https://poser.pugx.org/laravel/lumen-framework/d/total.svg)](https://packagist.org/packages/laravel/lumen-framework)
+[![Latest Stable Version](https://poser.pugx.org/laravel/lumen-framework/v/stable.svg)](https://packagist.org/packages/laravel/lumen-framework)
+[![Latest Unstable Version](https://poser.pugx.org/laravel/lumen-framework/v/unstable.svg)](https://packagist.org/packages/laravel/lumen-framework)
+[![License](https://poser.pugx.org/laravel/lumen-framework/license.svg)](https://packagist.org/packages/laravel/lumen-framework)
+
+Laravel Lumen is a stunningly fast PHP micro-framework for building web applications with expressive, elegant syntax. We believe development must be an enjoyable, creative experience to be truly fulfilling. Lumen attempts to take the pain out of development by easing common tasks used in the majority of web projects, such as routing, database abstraction, queueing, and caching.
+
+### Official Documentation
+
+Documentation for the framework can be found on the [Lumen website](http://lumen.laravel.com/docs).
+
+### Security Vulnerabilities
+
+If you discover a security vulnerability within Lumen, please send an e-mail to Taylor Otwell at taylor@laravel.com. All security vulnerabilities will be promptly addressed.
+
+### License
+
+The Lumen framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
---
+++ database/sql/project_manager_api.sql
@@ -0,0 +1,157 @@
+CREATE DATABASE  IF NOT EXISTS `project_manager_api` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `project_manager_api`;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: project_manager_api
+-- ------------------------------------------------------
+-- Server version	8.0.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `issues`
+--
+
+DROP TABLE IF EXISTS `issues`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `issues` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `project_id` int(10) unsigned NOT NULL,
+  `reporter` int(10) unsigned NOT NULL,
+  `assignee` int(10) unsigned NOT NULL,
+  `type` enum('TASK','IMPROVEMENT','STORY','GOAL','BUG','REQUIREMENT') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('OPEN','IN_PROGRESS','RESOLVED','CLOSED') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `priority` enum('BLOCKER','CRITICAL','MINOR','TRIVIAL') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `issues_project_id_foreign` (`project_id`),
+  KEY `issues_reporter_foreign` (`reporter`),
+  KEY `issues_assignee_foreign` (`assignee`),
+  CONSTRAINT `issues_assignee_foreign` FOREIGN KEY (`assignee`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `issues_project_id_foreign` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `issues_reporter_foreign` FOREIGN KEY (`reporter`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `issues`
+--
+
+LOCK TABLES `issues` WRITE;
+/*!40000 ALTER TABLE `issues` DISABLE KEYS */;
+/*!40000 ALTER TABLE `issues` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (13,'2018_10_05_143812_create_users_table',1),(14,'2018_10_05_143824_create_projects_table',1),(15,'2018_10_05_143947_create_issues_table',1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projects` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `projects_user_id_foreign` (`user_id`),
+  CONSTRAINT `projects_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('ACTIVE','INACTIVE') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'INACTIVE',
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `api_token` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`),
+  UNIQUE KEY `users_api_token_unique` (`api_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Esmeralda Rodriguez','esmeralda-rodriguez','INACTIVE','esmeralda@webtraining.zone','$2y$10$JIrDaHHTKogr3h21i0uqZuCwvSNsDcdoN17mxpLgax.fcA9oTDzoG',NULL,'jfj55lFpkciuIZyFsBY3MWfg9oEnhl3zfUpf5YHa487dI3NEdgxwwPnRs1d6','2018-10-09 00:43:57','2018-10-09 00:58:40'),(2,'Luis Ortíz','luis','INACTIVE','luis@webtraining.zone','$2y$10$1/RPamliSKh2hM9ojRx8su0/6W4Isy2KXR40SW27PmQoNzjUUjeR2',NULL,'EOru9g0xEpJTFgXT2cs69N9O2Lj6TjwJFFzFxKiJKcBHuEVk4UcU5mIZiIB0','2018-10-09 00:44:26','2018-10-09 13:57:48');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-09 15:14:07
